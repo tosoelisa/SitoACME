c126aac2d9d134c4c71f06566ebba24356608f10
tosoelisa <107490230+tosoelisa@users.noreply.github.com>
2023-02-27 16:54:31 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -2,18 +2,18 @@ function scrollToTop() {
   window.scrollTo(0, 0);
 }
 
-let popUp = document.getElementById("popup");
+let popup = document.getElementById("popup");
 let accetta = document.getElementById("accetta");
 let rifiuta = document.getElementById("rifiuta");
 
 function goUp() {
-  popUp.style = "transform: translateY(-125px);";
+  popup.style = "transform: translateY(-125px);";
   accetta.style = "transform: translateY(-175px);";
   rifiuta.style = "transform: translateY(-175px);";
 }
 
 function disappear() {
-  popUp.style = "display: none;";
+  popup.style = "display: none;";
   accetta.style = "display: none;";
   rifiuta.style = "display: none;";
 }
